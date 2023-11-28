ad5c4e9e1f54c3e2a00336b2753c4347e983575f
dev-vivekkr30 <dev.vivekkr30@gmail.com>
2023-11-28 19:56:55 +0530

    added-carousel and group-btn

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,34 @@
-$(document).ready(function() {
- 
-    $("#owl-demo").owlCarousel({
-      navigation : true
-    });
-   
-  });
+// carousle-js
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 150,
+
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
+});
+
+// work-section-btn-function
+
+// const active_work_btn = document.querySelector(".active-work");
+
+// let flag = 0;
+
+// active_work_btn.addEventListener('click', function onClick(change_color_btn){
+//     active_work_btn.style.background = "linear-gradient(90deg, #FE7F2D, #FFBF46)"
+// });
