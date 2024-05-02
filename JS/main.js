92b9f27c5ecf36bde7e8b8066bb2a58341c1dda5
dev-vivekkr30 <dev.vivekkr30@gmail.com>
2024-05-02 20:42:25 +0530

    added-owl-carousel

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,21 @@
-// carousle-js
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
+$('.owl-carousel').owlCarousel({
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 150,
-
-    // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-    el: '.swiper-scrollbar',
-    },
-});
-
-// work-section-btn-function
-
-// const active_work_btn = document.querySelector(".active-work");
-
-// let flag = 0;
-
-// active_work_btn.addEventListener('click', function onClick(change_color_btn){
-//     active_work_btn.style.background = "linear-gradient(90deg, #FE7F2D, #FFBF46)"
-// });
+    autoplay: true,
+    margin: 10,
+    dots: false,
+    slideTransition: 'linear',
+    autoplayTimeout: 1500,
+    autoplayHoverPause: false,
+    nav: false,
+    responsive: {
+        0: {
+            items: 4
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
